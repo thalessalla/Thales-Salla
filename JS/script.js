ce04f914af9dfaefec7cd588d7dd43edc2d2cba5
thalessalla <101899988+thalessalla@users.noreply.github.com>
2022-10-04 22:06:09 -0300

    commit 90%

--- JS/script.js
+++ JS/script.js
@@ -67,3 +67,23 @@ function scrollToSection(event) {
 linksInternos.forEach((link) => {
   link.addEventListener("click", scrollToSection);
 });
+
+// Menu burguer
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("ariel-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-labe", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-labe", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
